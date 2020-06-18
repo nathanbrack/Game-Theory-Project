8d7b3c095ad78b583d762427b8d49742d16659d7
nathanbrack <brack.nathan@hotmail.ch>
2020-06-18 14:37:18 +0200

    Add some more games

--- human_play_evaluation.py
+++ human_play_evaluation.py
@@ -12,19 +12,19 @@ def pkl_read(fp):
 
 def color(agent):
     if agent == "Uniform_random":
-        return 'gray'
+        return 'gray', 'Uniform random'
     elif agent == "Fixed_absolute_markovian":
-        return 'red'
+        return 'gray', 'Fixed Absolute Markovian'
     elif agent == "Fixed_relative_markovian":
-        return 'pink'
+        return 'gray', 'Fixed Relative Markovian'
     elif agent == "MW_absolute_markovian_eps0.5":
-        return 'green'
+        return 'darkgreen', 'Absolute Markovian Agent with epsilon = 0.5'
     elif agent == "MW_relative_markovian_eps0.5":
-        return 'blue'
+        return 'darkred', 'Relative Markovian Agent with epsilon = 0.5'
     elif agent == "MW_absolute_eps0.3":
-        return 'lightgreen'
+        return 'green', 'Absolute Markovian Agent with epsilon = 0.3'
     elif agent == "MW_relative_eps0.3":
-        return 'lightblue'
+        return 'red', 'Relative Markovian Agent with epsilon = 0.3'
 
 
 if __name__ == "__main__":
@@ -34,21 +34,40 @@ if __name__ == "__main__":
     games = logs.split(";")[:-1]
     logfile.close()
 
-    agents = []
-    for i in range(len(games)):
-    #if True: # just show last played game
-    #    i = len(games)-1 # just show last played game
-        fp = games[i]
-        agent = fp.split("--")[-1]
-        agents.append(agent)
-        game = pkl_read(f"./games/{fp}.pkl")
-        col = color(agent)
-        plt.plot(game.win_loss_ratio, col, label=agent)
+    # plot "confidence interval" grey area
+    M = 500  # number of simulations
+    N = 100  # number of rounds played each simulation
+    eps = 0.5  # tuning parameter: exploration (small eps) vs. exploitation (big eps)
+
+    for i in range(M):
+        p1 = Uniform_random(3)
+        p2 = Uniform_random(3)
+        game = RPSGame(p1, p2)
+
+        for j in range(N):
+            game.play_round()
+
+        print(f"Wins:{game.wins1}\tLosses:{game.losses1}\tTies:{game.ties}")
+        plt.plot(game.win_loss_ratio, 'lightgray')
 
     plt.ylim((0, 3))
     plt.xlabel('Number of Rounds')
     plt.ylabel('Win-Loss-ratio')
-    plt.plot([0, 100], [1, 1], 'black')  # plot horizontal line
+    plt.suptitle('Simulation of Rock Paper Scissors: MW Markovian vs. fixed Markovian')
+
+
+    agents = []
+    for i in range(len(games)):
+        fp = games[i]
+        agent = fp.split("--")[-1]
+        agents.append(agent)
+        if agent.find("MW_a") >= 0: # plot only MW-based agents
+            game = pkl_read(f"./games/{fp}.pkl")
+            col, lab = color(agent)
+            plt.plot(game.win_loss_ratio, col, label=lab)
+
+
+    plt.plot([0, 100], [1, 1], 'blue')  # plot horizontal line
     handles, labels = plt.gca().get_legend_handles_labels()
     by_label = dict(zip(labels, handles))
     plt.legend(by_label.values(), by_label.keys(), loc='upper right')
